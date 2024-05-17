9dfcb8f786d4e316aabd51670f84244826c2f87a
sr.pedrax <henriquepedrofariasdroliveira@gmail.com>
2024-05-17 09:39:29 -0300

    One Send

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware para parsing de JSON
+app.use(express.json());
+
+// Rota principal
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Iniciar o servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
